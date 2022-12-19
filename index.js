533747cd8280f31a09a213f91f2703bf77e83d1e
Aura <ax.pink@gmail.com>
2022-12-19 10:25:13 -0500

    commit tree 2b6084ba154f20a59abd999b69032b9ddf66f301

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+const submitData = (name, email) => {
+    const DOM = document.querySelector("body");
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        name,    
+        email,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        DOM.append(data.id);
+      })
+      .catch((message) => {
+        DOM.append(message);
+      });
+  };
+  
+  
+  
+  
+  
+  
+  
+  
